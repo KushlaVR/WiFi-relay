25f05e54c19dcf9c975ebc460f8d5195685c3df7
Виталий Кушла <KushlaVR@gmail.com>
2018-12-08 00:00:29 +0200

    Add support of mqtt setup file (mqtt.json)

--- MQTT-Relay/MQTT-Relay.ino
+++ MQTT-Relay/MQTT-Relay.ino
@@ -29,10 +29,10 @@ MIT license, all text above must be included in any redistribution
 
 /************************* Adafruit.io Setup *********************************/
 
-#define AIO_SERVER      "192.168.5.100"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    ""//"...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         ""//"...your AIO key..."
+//#define AIO_SERVER      "192.168.5.100"
+//#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+//#define AIO_USERNAME    ""//"...your AIO username (see https://accounts.adafruit.com)..."
+//#define AIO_KEY         ""//"...your AIO key..."
 
 /************ Global State (you don't need to change this!) ******************/
 
@@ -42,13 +42,13 @@ WiFiClient client;
 //WiFiClientSecure client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Client * mqtt = nullptr;// (&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 /****************************** Feeds ***************************************/
 
 // Setup a feed called 'photocell' for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
+Adafruit_MQTT_Publish * photocell;// = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
 
 // Setup a feed called 'onoff' for subscribing to changes.
 /*
@@ -65,15 +65,22 @@ optimistic: false
 qos: 0
 retain: true
 */
-Adafruit_MQTT_Publish onoffbutton_state = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/onoff");
-Adafruit_MQTT_Subscribe onoffbutton_set = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff/set");
-Adafruit_MQTT_Publish onoffbutton_available = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/onoff/available");
+Adafruit_MQTT_Publish * onoffbutton_state;// = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/onoff");
+Adafruit_MQTT_Subscribe * onoffbutton_set;// = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff/set");
+Adafruit_MQTT_Publish * onoffbutton_available;// = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/onoff/available");
+
+
+char * broker;
+const char * port;
+const char * user;
+const char * key;
 
 /*************************** Sketch Code ************************************/
 
 // Bug workaround for Arduino 1.6.6, it seems to need a function declaration
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
+void MQTT_ReadConfig();
 
 
 void setup() {
@@ -97,11 +104,20 @@ void setup() {
 
 	Serial.println(F("KushlaVR@gmail.com \nMQTT switch\n\n"));
 
-	
-	server.setup();
 
+	server.setup();
+	MQTT_ReadConfig();
+
+	Serial.print("broke:"); Serial.println(broker);
+	Serial.print("user:"); Serial.println(user);
+	Serial.print("key:"); Serial.println(key);
+
+	photocell = new Adafruit_MQTT_Publish(mqtt, "/feeds/photocell");
+	onoffbutton_state = new Adafruit_MQTT_Publish(mqtt, "/feeds/onoff");
+	onoffbutton_set = new Adafruit_MQTT_Subscribe(mqtt, "/feeds/onoff/set");
+	onoffbutton_available = new Adafruit_MQTT_Publish(mqtt, "/feeds/onoff/available");
 	// Setup MQTT subscription for onoff feed.
-	mqtt.subscribe(&onoffbutton_set);
+	mqtt->subscribe(onoffbutton_set);
 }
 
 uint32_t x = 0;
@@ -119,9 +135,9 @@ void loop() {
 		// try to spend your time here
 
 		Adafruit_MQTT_Subscribe *subscription;
-		while ((subscription = mqtt.readSubscription(1))) {
-			if (subscription == &onoffbutton_set) {
-				String value = String((char *)onoffbutton_set.lastread);
+		while ((subscription = mqtt->readSubscription(1))) {
+			if (subscription == onoffbutton_set) {
+				String value = String((char *)onoffbutton_set->lastread);
 				Serial.print(F("Got: "));
 				Serial.println(value);
 				if (value == "ON") {
@@ -140,7 +156,7 @@ void loop() {
 			Serial.print(F("\nSending photocell val "));
 			Serial.print(x);
 			Serial.print("...");
-			if (!photocell.publish(x++)) {
+			if (!photocell->publish(x++)) {
 				Serial.println(F("Failed"));
 			}
 			else {
@@ -173,7 +189,7 @@ void publish(Adafruit_MQTT_Publish * topic, const char * state) {
 
 void publish_available() {
 	// Now we can publish stuff!
-	if (!onoffbutton_available.publish("online")) {
+	if (!onoffbutton_available->publish("online")) {
 		Serial.println(F("available - Failed"));
 	}
 	else {
@@ -184,7 +200,7 @@ void publish_available() {
 
 void publish_state(const char * state) {
 	// Now we can publish stuff!
-	if (!onoffbutton_state.publish(state)) {
+	if (!onoffbutton_state->publish(state)) {
 		Serial.println(F("state - Failed"));
 	}
 	else {
@@ -192,23 +208,65 @@ void publish_state(const char * state) {
 	}
 }
 
+
+
+void MQTT_ReadConfig() {
+	if (SPIFFS.exists("/mqtt.json")) {
+		Serial.println(F("read ./mqtt.json"));
+		
+		DynamicJsonBuffer  jsonBuffer(200);
+		File f = SPIFFS.open("/mqtt.json", "r");
+		f.seek(0);
+		JsonObject& root = jsonBuffer.parseObject(f.readString());
+		if (root.containsKey("broker")) {
+			const char * b = root["broker"];
+			Serial.println(strlen(b));
+			broker = (char *)calloc(1,strlen(b)+1);
+			strncpy(broker, b, strlen(b));
+			
+			port = root["port"];
+			user = root["user"];
+			key = root["key"];
+			
+			uint16_t uPort = String(port).toInt();			
+			Serial.print("broker:"); Serial.println(broker);
+			Serial.print("port:"); Serial.println(uPort);
+			Serial.print("user:"); Serial.println(user);
+			Serial.print("key:"); Serial.println(key);
+
+			if (strlen(user) == 0) {
+				Serial.println("create mqtt");
+				mqtt = new Adafruit_MQTT_Client(&client, (const char *)broker, uPort);
+			}
+			else
+			{
+				mqtt = new Adafruit_MQTT_Client(&client, broker, uPort, user, key);
+			}
+			Serial.println("OK!");
+		}
+		f.close();
+	}
+}
+
+
 // Function to connect and reconnect as necessary to the MQTT server.
 // Should be called in the loop function and it will take care if connecting.
 void MQTT_connect() {
+	if (mqtt == nullptr) return;
 	int8_t ret;
 
 	// Stop if already connected.
-	if (mqtt.connected()) {
+	if (mqtt->connected()) {
 		return;
 	}
 
 	Serial.print("Connecting to MQTT... ");
 
 	uint8_t retries = 3;
-	while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-		Serial.println(mqtt.connectErrorString(ret));
+	while ((ret = mqtt->connect()) != 0) { // connect will return 0 for connected
+		Serial.println(mqtt->connectErrorString(ret));
 		Serial.println("Retrying MQTT connection in 5 seconds...");
-		mqtt.disconnect();
+		mqtt->disconnect();
 		delay(5000);  // wait 5 seconds
 		retries--;
 		if (retries == 0) {
