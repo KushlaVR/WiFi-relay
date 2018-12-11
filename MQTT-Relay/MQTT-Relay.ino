39ae0c0bb22b9b4129fa2cffba728d352018c53b
Віталій Кушла <KushlaVR@gmail.com>
2018-12-11 09:56:37 +0200

    add firmvare update example to the end of file...

--- MQTT-Relay/MQTT-Relay.ino
+++ MQTT-Relay/MQTT-Relay.ino
@@ -152,4 +152,43 @@ void loop() {
 	}
 }
 
+/*
 
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  SPIFFS.begin();
+  Dir dir = SPIFFS.openDir("/");
+
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
+
+  File file = SPIFFS.open("/blinkESP.bin", "r");
+
+
+  uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+  if (!Update.begin(maxSketchSpace, U_FLASH)) { //start with max available size
+    Update.printError(Serial);
+    Serial.println("ERROR");
+  }
+ 
+  while (file.available()) {
+    uint8_t ibuffer[128];
+    file.read((uint8_t *)ibuffer, 128);
+    Serial.println((char *)ibuffer);
+    Update.write(ibuffer, sizeof(ibuffer));
+  
+  }
+
+  
+  Serial.print(Update.end(true));
+  digitalWrite(BUILTIN_LED, HIGH);
+  file.close();
+}
+
+void loop() {
+  
+}
+
+
+*/
