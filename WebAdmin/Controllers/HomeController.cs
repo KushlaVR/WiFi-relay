0d12288f5028c8161bc8396caeea7ce4fac33576
Виталий Кушла <KushlaVR@gmail.com>
2019-02-21 09:56:36 +0200

    Add lib manager

--- WebAdmin/Controllers/HomeController.cs
+++ WebAdmin/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace WebAdmin.Controllers
             return Redirect("index.html");
         }
 
-
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
