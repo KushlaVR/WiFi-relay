25092f58bc871ca9c387279b65093fd6134f8836
Виталий Кушла <KushlaVR@gmail.com>
2020-03-26 21:00:08 +0200

    Add project for sensor testing

---
+++ SensorsTest/SensorsTest.ino
@@ -0,0 +1,35 @@
+﻿/*
+ Name:		SensorsTest.ino
+ Created:	3/25/2020 10:24:25 PM
+ Author:	Віталік
+*/
+
+#include <Wire.h>
+#include "Adafruit_Sensor.h"
+#include <Adafruit_AM2320.h>
+
+#define pinSDA D2
+#define pinSCL D1
+//#define AM2320 0xB8
+
+TwoWire i2c_bus = TwoWire();
+Adafruit_AM2320 sensor = Adafruit_AM2320(&i2c_bus);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(115200);
+	i2c_bus.begin(pinSDA, pinSCL);
+	sensor.begin();
+}
+
+ulong lastRead = 0;
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	if (lastRead == 0 || (millis() - lastRead) > 2000) {
+		float h = sensor.readHumidity();
+		float t = sensor.readTemperature();
+		Serial.printf("T = %f, H = %f\n", t, h);
+		lastRead = millis();
+	}
+}
